e52956bc342a1b7ff129b334bb148c0fe0eb0d0b
Antonio Pérez <apema2079@gmail.com>
2021-11-30 03:32:40 -0600

    Handle user input to change snake direction

--- src/views/ruby2d.rb
+++ src/views/ruby2d.rb
@@ -4,8 +4,9 @@ require_relative '../model/state'
 
 module View
   class Ruby2dView
-    def initialize
+    def initialize(app)
       @pixel_size = 50
+      @app = app
     end
 
     def start(state)
@@ -14,6 +15,9 @@ module View
         title: "Snake", 
         width: @pixel_size * state.grid.cols, 
         height: @pixel_size * state.grid.rows)
+      on :key_down do |event|
+        handle_key_event(event)
+      end
       show
     end
 
@@ -48,6 +52,19 @@ module View
           size: @pixel_size,
           color: 'green',
         )
+      end
+    end
+
+    def handle_key_event(event)
+      case event.key
+      when "up"
+        @app.send_action(:change_direction, Model::Direction::UP)
+      when "down"
+        @app.send_action(:change_direction, Model::Direction::DOWN)
+      when "left"
+        @app.send_action(:change_direction, Model::Direction::LEFT)
+      when "right"
+        @app.send_action(:change_direction, Model::Direction::RIGHT)
       end
     end
   end
